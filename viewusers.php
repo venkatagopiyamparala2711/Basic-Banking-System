f3532117ba37f5e1333f1f457b3663c6d3a333c7
Venkata Gopi Yamparala <56405372+venkatagopiyamparala2711@users.noreply.github.com>
2020-12-21 12:21:59 +0530

    Update viewusers.php

--- viewusers.php
+++ viewusers.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Venkat's Basic banking System</title>
+    <title>Venkat's normal banking System</title>
     <!-- <link rel="stylesheet" href="./style.css"> -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
